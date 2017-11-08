7f0f68e5142f1ff004c3f26c137a75fa065ae4fb
Jasmine Cervantes <jt24cervantes@yahoo.com>
2017-11-08 11:37:55 -0700

    update

--- app/inbox/component.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-  angular
-    .module('angular1.6-inbox')
-    .component('inbox', {
-      controller:'inboxController',
-      templateUrl:'app/inbox/inbox-temp.html'
-    })
-}());
--- js/main.js
+++
@@ -1,6 +0,0 @@
-(function (){
-  'use strict';
-
-  angular
-    .module('angular1.6-inbox', [])
-})();
